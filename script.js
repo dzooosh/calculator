4a8fff499118f9c12dee102a9a9931572a86b1f1
Alien <alongejoshua@gmail.com>
2023-08-25 18:03:06 +0100

    ensures the calculator is up and running

--- script.js
+++ script.js
@@ -22,11 +22,13 @@ function divide(a, b) {
 // console.log(ans);
 
 // function operate that takes the variables and call above funcions
-function operate(operator, fn, sn) {
+function operate(arg) {
+    let fn = arg[0];
+    let operator = arg[1];
+    let sn = arg[2];
     // convert string to number
     fn = parseInt(fn);
     sn = parseInt(sn);
-    
     let result = 0;
     switch(operator) {
         case '+':
@@ -38,14 +40,14 @@ function operate(operator, fn, sn) {
         case '*':
             result = multiply(fn, sn);
             break;
-        case '/':
+        case '÷':
             result = divide(fn, sn);
             break;
             default:
                 result = "invalid operator";
             }
-            
-    return result;
+    
+    return result.toString().slice(0, 12);
     
 }
 
@@ -58,38 +60,94 @@ function addGlobalEventListener(type, selector, callback) {
 
 
 const displayValue = document.querySelector('.value');
-const button = document.querySelector('.button');
+const operations = document.querySelectorAll('.buttons.op')
+const buttons = document.querySelectorAll('.button');
+const upDisplay = document.querySelector('.exp');
 let t1 = document.createElement('h1');
-let total, fn, sn = 0;
+let up = document.createElement('h5');
+let total;
+let allValue = '0';
+let output = '0';
+const operatorList = ['+', '-', '÷', '*', '=']
+let operator;
 
-// populates the display with number buttons
-function populate(e) {
-    console.log(e.target.textContent);
-    let text = e.target.textContent;
-    t1.textContent += text;
-    displayValue.append(t1);
-}
-// number and operation buttons
-addGlobalEventListener('click', '.button', e => populate(e));
+
+t1.textContent = '0';
+displayValue.append(t1);
+
+// the operator buttons
+operations.forEach(op => {
+    op.addEventListener("click", (e) => {
+        
+        // check if '=' was passed
+        cal = output.split(' ').filter(item => item !== '');
+        if (cal.length === 3) {
+            console.log(cal)
+            total = operate(cal);
+            console.log(total);
+            t1.textContent = total;
+            output = total;
+            allValue = total;
+        }
+
+        //check if a operator has been selected
+        if (operatorList.indexOf(allValue.slice(-1)) !== -1) {
+            operator = e.target.textContent;
+            // operation change if clicked before
+            if (allValue.slice(-1) !== operator) {
+                allValue = allValue.slice(0, -1);
+                allValue += operator;
+                output = output.slice(0, -3)
+                output += ' ' + operator + ' ';
+            }  
+        } else {
+            operator = e.target.textContent;
+            allValue += operator;
+            output += " " + operator + " ";
+        }
+        
+    })
+});
+
+// number buttons
+addGlobalEventListener('click', '.button', e => {
+    // check if an operator has been clicked and renew number display
+    if (operatorList.indexOf(allValue.slice(-1)) !== -1) {
+        t1.textContent = '';
+    }
+    if (t1.textContent.length <= 11) {
+        // checking if the displayvalue == 0 when a number is pressed
+        if (t1.textContent === '0') {
+            t1.textContent = '';
+            output = '';
+            allValue = '';
+        }
+        allValue += e.target.textContent;
+        output += e.target.textContent;
+        t1.textContent += e.target.textContent;
+        
+        console.log(allValue)
+        console.log(output)
+        displayValue.append(t1)
+    }
+});
 
 function clear() {
-        t1.textContent = '';
+    t1.textContent = '0';
+    allValue = '0';
+    output = '0';
 };
+
 // clear button 
 addGlobalEventListener('click', '#clear', clear);
-
-// equals button
-addGlobalEventListener('click', '#equals', e => {
-   
-});
 
 // delete button
 addGlobalEventListener('click', '#delete', e => {
     let del = t1.textContent;
     t1.textContent = del.slice(0, -1);
+    allValue = allValue.slice(0, -1);
+    output = output.slice(0, -1);
 });
 
-// clear button
-
-
-// fn operator sn total operator number total
+// up.textContent = allValue;
+// upDisplay.append(up);
